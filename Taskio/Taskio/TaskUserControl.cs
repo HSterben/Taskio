152d6e8b7f785d473839a00388a2c56d70a5ccce
Zlatin Tsvetkov <zlatintsvetkov@gmail.com>
2023-12-02 13:30:31 -0500

    attach editing and deletion to GUI

--- Taskio/Taskio/TaskUserControl.cs
+++ Taskio/Taskio/TaskUserControl.cs
@@ -22,9 +22,22 @@ namespace Taskio
             this.taskName.Text = name;
             this.priority.Text = Convert.ToString(priority);
             this.Description = description;
-
-            
+           
             this.Click += TaskUserControl_Click;
+            AttachClickEventToChildren(this);
+        }
+        private void AttachClickEventToChildren(Control parent)
+        {
+            foreach (Control child in parent.Controls)
+            {
+                child.Click += TaskUserControl_Click;
+
+                // Recursively attach to children of the child, if any
+                if (child.HasChildren)
+                {
+                    AttachClickEventToChildren(child);
+                }
+            }
         }
         private void TaskUserControl_Click(object sender, EventArgs e)
         {
